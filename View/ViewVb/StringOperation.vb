07f28076ecdc05d8acccee69f8b7a5b5153b46e7
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-05-05 19:58:05 +0900

    関数とサブプロシージャの宣言をコピー

--- View/ViewVb/StringOperation.vb
+++ View/ViewVb/StringOperation.vb
@@ -30,6 +30,136 @@ Dim strText As String
     ByteToString = strText
 End Function
 
+Public Function DeleteFileExt(ByVal strFileName As String) As String
+'--------------------------------------------------------------------
+'ファイル名から、拡張子のみを除いた部分を取得する
+'返される結果には、パス名が含まれる
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function DeleteFilePath(ByVal strFileName As String) As String
+'--------------------------------------------------------------------
+'ファイル名から、パス名を除いた部分を取得する
+'返される結果には、拡張子も含むが、パスの区切りを示す \ は含まない
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function GetFileExt(ByVal strFileName As String) As String
+'--------------------------------------------------------------------
+'ファイルの拡張子を取得する
+'この拡張子には、先頭の "." を含まない
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function GetFilePath(ByVal strFileName As String) As String
+'--------------------------------------------------------------------
+'ファイルのパスを取得する
+'このパス名には、最後の \ を含まない
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function GetFileTitle(ByVal strFileName As String) As String
+'--------------------------------------------------------------------
+'ファイルのタイトル、つまりファイル名から
+'パスと拡張子を除いた残りの部分を取得する
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function GetFullPathName(ByVal strPathName As String, ByVal strFileName As String) As String
+'--------------------------------------------------------------------
+'strPathNameを基準とした、相対パスから、フルパスを取得する
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function GetRelativePath(ByVal strFileName As String, ByVal strPathName As String) As String
+'--------------------------------------------------------------------
+'フルパスから、strPathNameを基準とした、相対パスを取得する
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function HexToLong(ByVal strHex As String) As Long
+'--------------------------------------------------------------------
+'文字列として書かれた16進数を整数に変換する
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function HexToSingle(ByVal strHex As String) As Single
+'--------------------------------------------------------------------
+'文字列として書かれた16進数を単精度浮動小数点(Single)に変換する
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function MakeHex(ByVal X As Long, ByVal nLen As Long, Optional ByVal chDigit = " ") As String
+'--------------------------------------------------------------------
+'Xを16進数に変換し、余った桁はchDigitで埋めてnLen文字分にする
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function ParseString(ByRef lpszText As String, ByVal strSep As String) As String
+'--------------------------------------------------------------------
+'文字列を解析する
+'lpszTextを先頭から走査し、strSepで区切る。
+'strSepの手前までを返し、lpszTextをstrSep以降から始まるように調整する
+'--------------------------------------------------------------------
+
+End Function
+
+Public Sub Pause(ByVal sngSeconds As Single)
+'--------------------------------------------------------------------
+'指定した時間だけ待機する
+'メッセージは処理する
+'--------------------------------------------------------------------
+
+End Sub
+
+Public Function ProcessEscapeSequence(ByVal strText As String) As String
+'--------------------------------------------------------------------
+'文字列中のエスケープシーケンスを処理する
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function ReplaceConstant(ByVal strText As String, ByRef strConstName() As String, ByRef ConstValue() As String) As String
+'--------------------------------------------------------------------
+'文字列中の定数名に値を代入して返す
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function ReplaceSpaceChar(ByVal strText As String, Optional ByVal strChar As String = " ", Optional ByVal blnWideSpace As Boolean = False) As String
+'--------------------------------------------------------------------
+'文字列の先頭から、空白文字(連続する、半角スペース、タブ)を
+'指定された文字列に置き換える。デフォルトでは半角スペースに置き換える
+'blnWideSpaceがTrueならば、全角スペースも対象にする
+'--------------------------------------------------------------------
+
+End Function
+
+Public Function ReplaceValueToConstantName(ByVal strText, ByVal strConstValue As String, ByVal strConstName As String, ByVal blnCheckCase As Boolean) As String
+'------------------------------------------------------------------------------
+'文字列中の特定の文字列を、定数名で置き換える
+'CheckCaseがTrueならば、大文字と小文字を異なるものとして扱う
+'------------------------------------------------------------------------------
+
+End Function
+
+Public Function SetDefaultText(ByVal strDefaultFile As String) As String
+'--------------------------------------------------------------------
+'ファイルからデフォルトのテキストを読み込む
+'--------------------------------------------------------------------
+
+End Function
+
 Public Function StringToByte(ByVal strText As String, _
         ByRef lpBuf() As Byte,
         ByVal lngStart As Long, ByVal lngEnd As Long, _
