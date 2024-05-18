e9aee739dc453f201c7e73ff74722167c45f60cf
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-05-18 20:27:46 +0900

    定数宣言をコピー

--- View/ViewVb/Answer.vb
+++ View/ViewVb/Answer.vb
@@ -1,3 +1,11 @@
 ﻿Module Answer
 
+Public Const SQUARE_BLANK As Long = &H80&
+Public Const SQUARE_ASSUM As Long = &H40&
+Public Const SQUARE_CHECK As Long = &H20&
+Public Const SQUARE_COLOR As Long = &HF&
+
+Public Const MAX_HINTS_PER_LINE As Long = 64
+Public Const MAX_SQUARES_PER_LINE As Long = 256
+
 End Module
