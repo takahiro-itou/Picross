a4fe61ed3134b397399dd0f184529391f6e5c7af
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-05-18 20:28:48 +0900

    構造体宣言をコピー

--- View/ViewVb/Answer.vb
+++ View/ViewVb/Answer.vb
@@ -8,4 +8,15 @@ Public Const SQUARE_COLOR As Long = &HF&
 Public Const MAX_HINTS_PER_LINE As Long = 64
 Public Const MAX_SQUARES_PER_LINE As Long = 256
 
+Public Type tPicrossHint
+    nCount As Long
+    nNumbers(0 To MAX_HINTS_PER_LINE - 1) As Long
+    nColors(0 To MAX_HINTS_PER_LINE - 1) As Long
+End Type
+
+Public Type tPicrossLine
+    nSquares(0 To MAX_SQUARES_PER_LINE - 1) As Byte
+    nBlockIndex(0 To MAX_SQUARES_PER_LINE - 1) As Long
+End Type
+
 End Module
