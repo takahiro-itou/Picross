79ddcee9a69f67f56c53268d1eecb139977acbd1
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-06-01 23:28:34 +0900

    関数とサブプロシージャの宣言をコピー

--- View/ViewVb/Picross.vb
+++ View/ViewVb/Picross.vb
@@ -7,4 +7,56 @@ Public gstrAppPath As String
 ' Public gobjfMainForm As frmGame
 Public gobjfMainForm As System.Windows.Forms.Form
 
+Public Function LoadGameData(ByRef lpGame As CPicross, ByVal strFileName As String) As Boolean
+'------------------------------------------------------------------------------
+'ファイルから問題データを読み込む
+'ファイルのフォーマットは１行目を見て自動的に識別する
+'------------------------------------------------------------------------------
+
+End Function
+
+Public Function LoadGameDataFromIniFile(ByRef lpGame As CPicross, ByVal strFileName As String) As Boolean
+'------------------------------------------------------------------------------
+'INI 形式のファイルから問題データを読み込む
+'------------------------------------------------------------------------------
+
+End Function
+
+Public Function LoadGameDataFromStandardFile(ByRef lpGame As CPicross, ByVal lngFileNumber As Long) As Boolean
+'------------------------------------------------------------------------------
+'標準形式のファイルから、問題データを読み込む
+'------------------------------------------------------------------------------
+
+End Function
+
+Public Function LoadGameStatus(ByRef lpGame As CPicross, ByVal strFileName As String, _
+    ByRef lpCursorX As Long, ByRef lpCursorY As Long) As Boolean
+'------------------------------------------------------------------------------
+'ファイルから局面をロードする
+'------------------------------------------------------------------------------
+
+End Function
+
+Public Sub Main()
+'------------------------------------------------------------------------------
+'プログラムのエントリポイント
+'------------------------------------------------------------------------------
+
+End Sub
+
+Public Function ParseCommandLine(ByVal strCommandLine As String) As Boolean
+'------------------------------------------------------------------------------
+'コマンドライン引数を解析する
+'------------------------------------------------------------------------------
+
+End Function
+
+Public Function SaveGameStatus(ByRef lpGame As CPicross, ByVal strFileName As String, _
+    ByVal nCursorX As Long, ByVal nCursorY As Long) As Boolean
+'------------------------------------------------------------------------------
+'現在の局面をファイルにセーブする
+'------------------------------------------------------------------------------
+
+End Function
+
 End Module
