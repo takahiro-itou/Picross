268586a607b78bfbe68a2a2a2552a754a4d57284
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-06-03 22:03:00 +0900

    クラスのメンバ変数の宣言をコピー

--- View/ViewVb/CPicross.vb
+++ View/ViewVb/CPicross.vb
@@ -1,3 +1,33 @@
 Public Class CPicross
 
+'ゲームID
+Private mlngGameID As Long
+
+'背理法モード
+Private mlngTestModeLevel As Long
+
+'フィールド
+Private mlngFieldCols As Long
+Private mlngFieldRows As Long
+Private mlngSquares() As Long
+
+'ヒントデータ
+Private mutTateHints() As tPicrossHint       '縦方向のヒント
+Private mutYokoHints() As tPicrossHint       '横方向のヒント
+
+'もっとも長いヒントデータ
+Private mlngMaxTateHintsLength As Long
+Private mlngMaxYokoHintsLength As Long
+
+Private mlngRestSquares As Long
+
+Private Const mlngFieldLeftMargin As Long = 2
+Private Const mlngFieldTopMargin As Long = 2
+Private mlngSquareWidth As Long
+Private mlngSquareHeight As Long
+
+'ヒントを表示するのに必要な領域
+Private mlngHintAreaWidth As Long
+Private mlngHintAreaHeight As Long
+
 End Class
