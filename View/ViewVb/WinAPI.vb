2bc93e3e8210a1918fb2368297acb96dc8e5e360
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-06-22 17:00:58 +0900

    関数の宣言をコピー

--- View/ViewVb/WinAPI.vb
+++ View/ViewVb/WinAPI.vb
@@ -10,3 +10,29 @@
 ''          or (at your option) any later version.                      ''
 ''                                                                      ''
 ''************************************************************************
+
+Imports System.Runtime.InteropServices
+
+Module WinAPI
+
+''  設定をファイルから読み込む。
+<DllImport("KERNEL32.DLL", CharSet:=CharSet.Auto)>
+Public Function GetPrivateProfileString(
+        ByVal lpAppName As String,
+        ByVal lpKeyName As String,
+        ByVal lpDefault As String,
+        ByVal lpReturnedString As System.Text.StringBuilder,
+        ByVal nSize As Integer,
+        ByVal lpFileName As String) As Integer
+End Function
+
+''  設定をファイルに保存する。
+<DllImport("KERNEL32.DLL")>
+Public Function WritePrivateProfileString(
+        ByVal lpAppName As String,
+        ByVal lpKeyName As String,
+        ByVal lpString As String,
+        ByVal lpFileName As String) As Integer
+End Function
+
+End Module
