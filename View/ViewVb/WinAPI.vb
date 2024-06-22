8d9a73a5d3b30a4e37f08ae0197d9fd399d6ab00
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-06-22 16:59:48 +0900

    空のファイルを新規作成

---
+++ View/ViewVb/WinAPI.vb
@@ -0,0 +1,12 @@
+''************************************************************************
+''                                                                      ''
+''                  ---   Picross Solver   View.   ---                  ''
+''                                                                      ''
+''          Copyright (C), 2024, Takahiro Itou                          ''
+''          All Rights Reserved.                                        ''
+''                                                                      ''
+''          License: (See COPYING and LICENSE files)                    ''
+''          GNU Affero General Public License (AGPL) version 3,         ''
+''          or (at your option) any later version.                      ''
+''                                                                      ''
+''************************************************************************
